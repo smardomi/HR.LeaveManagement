6269a7a98da9a12ea915bea7366112f44c93d57a
Saeed Mardomi <saeedmardomi@hotmail.com>
2022-09-04 13:01:39 +0430

    Update Mapper Profile

--- src/Core/HR.LeaveManagement.Application/Profiles/MappingProfile.cs
+++ src/Core/HR.LeaveManagement.Application/Profiles/MappingProfile.cs
@@ -10,10 +10,21 @@ namespace HR.LeaveManagement.Application.Profiles
     {
         public MappingProfile()
         {
+            #region LeaveRequest Mappings
             CreateMap<LeaveRequest, LeaveRequestDto>().ReverseMap();
-            CreateMap<LeaveRequest, LeaveRequestListDto>().ReverseMap();
-            CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
+            CreateMap<LeaveRequest, LeaveRequestListDto>()
+                .ForMember(dest => dest.DateRequested, opt => opt.MapFrom(src => src.DateCreated))
+                .ReverseMap();
+            CreateMap<LeaveRequest, CreateLeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveRequest, UpdateLeaveRequestDto>().ReverseMap();
+            #endregion LeaveRequest
+
             CreateMap<LeaveAllocation, LeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveAllocation, CreateLeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveAllocation, UpdateLeaveAllocationDto>().ReverseMap();
+
+            CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
+            CreateMap<LeaveType, CreateLeaveTypeDto>().ReverseMap();
         }
     }
 }
